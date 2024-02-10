9764ef07ad02a66e4174fa8da97857c5570c0b45
Balaji Manoharan <balaji27391@gmail.com>
2024-02-10 11:23:10 +0700

    Add project files.

---
+++ ForeignCurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class CurrencyConverter
+{
+    public string ForeignCurrencyConverter(string currency)
+    {
+        return "Rs.85";
+    }
+}
